ef982df7c21b9b37405cde6e02a52b6a4d5d297f
Justin George <justin.george@gmail.com>
2022-06-28 01:21:27 -0700

    Trailing whitespace errors galore - can now hello world in python

--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,21 @@ use std::cmp;
 
 #[pyfunction]
 #[pyo3(text_signature = "(s)")]
-fn read_lines_from_file(path: &str, lineno: usize, context_lines: usize) -> Option<&str> {
+fn read_lines_from_file(path: &str, lineno: usize, context_lines: usize) -> () {
     let reader = io::BufReader::new(File::open("input").expect("Cannot open file"));
     let start = cmp::max(0, lineno - context_lines);
-    println!("Start: {}", start)
+    println!("something spicy")
+    
+}
+
+#[pyfunction]
+fn do_a_hello_world() -> PyResult<String> {
+    Ok("hello world".to_string())
 }
 
 #[pymodule]
 fn elasticapmspeedups(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(read_lines_from_file, m)?)?;
+    m.add_function(wrap_pyfunction!(do_a_hello_world, m)?)?;
     Ok(())
 }
