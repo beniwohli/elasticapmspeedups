8a4f2baf8f418dbed8775ca1d0afbfdb73534e43
Justin George <justin.george@gmail.com>
2022-06-28 14:21:13 -0700

    Actually return the lines to python

--- src/lib.rs
+++ src/lib.rs
@@ -6,23 +6,20 @@ use std::cmp;
 
 #[pyfunction]
 #[pyo3(text_signature = "(s)")]
-fn read_lines_from_file(path: &str, lineno: usize, context_lines: usize) -> () {
+fn read_lines_from_file(path: &str, lineno: usize, context_lines: usize) -> Vec<String> {
     let reader = io::BufReader::new(File::open(path).expect("Cannot open file"));
     let start = cmp::max(0, lineno - context_lines);
     let lines_iter = reader.lines().skip(start).take(context_lines * 2 + 1).map(|l| l.unwrap());
+    let mut lines = Vec::<String>::new();
     for line in lines_iter {
-        println!("{}",line);
+        lines.push(line.to_string())
     }
+    lines
 }
 
-#[pyfunction]
-fn do_a_hello_world() -> PyResult<String> {
-    Ok("hello world".to_string())
-}
 
 #[pymodule]
 fn elasticapmspeedups(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(read_lines_from_file, m)?)?;
-    m.add_function(wrap_pyfunction!(do_a_hello_world, m)?)?;
     Ok(())
 }
--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 import elasticapmspeedups
 # should output lines 9 through 13 to stdout
-elasticapmspeedups.read_lines_from_file('input', 10, 2)
+print(elasticapmspeedups.read_lines_from_file('input', 10, 2))
