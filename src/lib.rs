d3cd129285c7ad37e0d5ff7548b324445f8b3b7a
Justin George <justin.george@gmail.com>
2022-06-28 20:37:16 -0700

    Begin converting more of stacks.py to rust

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 use std::cmp;
+use regex::Regex;
 
 #[pyfunction]
 #[pyo3(text_signature = "(s)")]
@@ -17,6 +18,24 @@ fn read_lines_from_file(path: &str, lineno: usize, context_lines: usize) -> Vec<
     lines
 }
 
+
+
+fn is_library_frame(absolute_path: String, include_paths: Vec<String>, exclude_paths: Vec<String>) -> bool {
+    if (!include_paths.is_empty()) && get_path_regex(include_paths).is_match(&absolute_path) {
+        return false
+    } else {
+        if (!exclude_paths.is_empty()) && get_path_regex(exclude_paths).is_match(&absolute_path) {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+// this will need to do something better, like construct regex only once for each set of paths and return a reference
+fn get_path_regex(paths: Vec<String>) -> Regex {
+    Regex::new(&paths.join("|")).unwrap()
+}
 
 #[pymodule]
 fn elasticapmspeedups(_py: Python, m: &PyModule) -> PyResult<()> {
